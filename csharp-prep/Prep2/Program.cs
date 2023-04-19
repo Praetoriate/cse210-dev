f622e9b3450ec740be2736f0df0e96ff9f8c6895
Praetoriate <btravis.elm@gmail.com>
2023-04-18 20:31:40 -0700

    W1 activities, prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage (##)? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade); 
+        
+        string letter = "";
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+
+        if (number <90 && number >= 80)
+        {
+            letter = "B";
+        }
+
+        if (number <80 && number >= 70)
+        {
+            letter = "C";
+        }
+
+        if (number <70 && number >= 60)
+        {
+            letter = "D";
+        }
+
+        if (number <60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You received the grade: {letter}.");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class. Try again.");
+        }
     }
 }
